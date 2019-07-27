e9ca7e71e7668e4647a3d8a24e8a3888d629b178
Sandra <100317575@alumnos.uc3m.es>
2019-07-27 19:30:47 +0200

    Main Menu Cute vAnimal

---
+++ VRStorytelling/Assets/Scripts/MainMenuHandler.cs
@@ -0,0 +1,37 @@
+﻿using System;
+using System.Collections;
+using UnityEngine;
+using UnityEngine.SceneManagement;
+using UnityEngine.XR;
+
+public class PlayButtonHandler : MonoBehaviour
+{
+    // Start is called before the first frame update
+    void Start()
+    {
+        StartCoroutine(LoadDevice("None"));
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+
+    // Cambiar a elige una escena
+    public void DoOnPlayClick()
+    {
+        SceneManager.LoadScene("ForestScene");
+    }
+
+    //Change device view mode
+    public IEnumerator LoadDevice(string newDevice)
+    {
+        if (String.Compare(XRSettings.loadedDeviceName, newDevice, true) != 0)
+        {
+            XRSettings.LoadDeviceByName(newDevice);
+            yield return null;
+            XRSettings.enabled = true;
+        }
+    }
+}
