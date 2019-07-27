31ac397fe53f5e010ae60ee778e83c27e2df720d
Sandra <100317575@alumnos.uc3m.es>
2019-07-27 18:08:32 +0200

    Change view from Menu to Scene

---
+++ VRStorytelling/Assets/Scripts/MainCameraForest.cs
@@ -0,0 +1,29 @@
+﻿using System;
+using System.Collections;
+using UnityEngine;
+using UnityEngine.XR;
+
+public class MainCameraForest : MonoBehaviour
+{
+    // Start is called before the first frame update
+    void Start()
+    {
+        StartCoroutine(LoadDevice("Cardboard"));
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+
+    public IEnumerator LoadDevice(string newDevice)
+    {
+        if (String.Compare(XRSettings.loadedDeviceName, newDevice, true) != 0)
+        {
+            XRSettings.LoadDeviceByName(newDevice);
+            yield return null;
+            XRSettings.enabled = true;
+        }
+    }
+}
